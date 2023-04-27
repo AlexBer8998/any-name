03d21ba056825ff23116792aee3c3f4b0175b00e
alexander <kartinatvberlin@gmail.com>
2023-04-27 18:58:53 +0200

    new

---
+++ script.js
@@ -0,0 +1,126 @@
+
+
+
+// const num1 = 20
+// const num2 = 50
+// console.log('Peremena 1', num1)
+// console.log('Peremena 2', num2)
+// console.log('cuma', num1 + num2)
+
+// const num3 = 20
+// const num4= 50
+// console.log('Peremena 1', num3)
+// console.log('Peremena 2', num4)
+// console.log('cuma', num3 + num4)
+
+// function showVariablesSum(num1,num2) {
+//     console.log('Peremena 1', num1)
+//     console.log('Peremena 2', num2)
+//     console.log('cuma', num1 + num2)
+
+// }
+// showVariablesSum() // вызов функции
+// showVariablesSum() // вызов функции
+
+ // Задача. 
+// function calc(x, y) {
+//     console.log(x + y)
+//     console.log(x - y)
+//     console.log(x * y)
+//     console.log(x / y)
+// }
+// calc(100, 20)
+// calc(2, 4)
+
+
+// Задача. Создать функцию 
+// S = a + b.
+// function calcRoomSquare(a, b){
+//     const s = a * b
+//     console.log(`длина комноты = ${a}, ширина = ${b}. Площадь = ${s}`)
+// }
+// calcRoomSquare(20, 10)
+
+// function calcRoomSquare(a, b) {
+//     const s = a * b
+//     console.log(`Длина комнаты = ${a}, ширина = ${b}. Площадь = ${s}`)
+// }
+
+// const length = +prompt("Введите длину комнаты")
+// const width = +prompt("Введите ширину комнаты")
+// calcRoomSquare(length, width)
+
+
+// function schowNameUpperCase(name) {
+//     console.log(name.toUpperCase())
+// }
+// const userName = prompt("NAME")
+// schowNameUpperCase("Arsen")
+// schowNameUpperCase("Maria")
+
+
+// function showArrayElements(array) {
+//     for(let i = 0; i < Array.length; i++) {
+//         console.log(array[i])
+//     }
+//  }
+//  const numbers = [10, 20, 30, 40, 50]
+//  showArrayElements(numbers)
+//  showArrayElements(["apple", "orange", "cherry", "peach"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Задача . Создать функцию , которая должна принимать число . умножть его на 2, возводить в 3ю степень
+
+function calc(namber) {
+    const result = (number * 2) ** 3 - number
+    return result
+} 
+console.log(result)
+const myNuber = calc(10)
+console.log(myNuber)
+alert(myNuber)
+
+
+// Создать футкцию , которая принемает два числа . возводит их в квадрат , сумиирует и возврощает результат
+// выполнение этих математических действий. 2, 4, -> 
+/*
+     1.
+     2.
+     3.
+     4.
+     5.
+ */
